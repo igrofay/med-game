2f06e06f5580ec6b6fcb812fcec98f3aea92658e
Сергей Никитин <sergejnikitin@MacBook-Pro-Sergej.local>
2022-11-20 22:24:40 +0500

    изменения для И

---
+++ app/src/main/java/ru/okei/med/feature/profile_screen/view/DepartmentInfo.kt
@@ -0,0 +1,9 @@
+package ru.okei.med.feature.profile_screen.view
+
+import androidx.compose.runtime.Composable
+import ru.okei.med.domain.model.Department
+
+@Composable
+fun Department(label: Department) {
+
+}
